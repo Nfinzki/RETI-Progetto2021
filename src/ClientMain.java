b223d40e664d9c769fb9a7cc7d1fcf518810a704
Nfinzki <francescodiluzio07@gmail.com>
2021-12-17 11:17:18 +0100

    Implemented registration process from CLI

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -5,6 +5,10 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class ClientMain {
@@ -12,7 +16,7 @@ public class ClientMain {
 
     private static int registryPort = 11111; //TODO Mettere una porta di default più significativa
     private static String registryHost = "localhost";
-    private static String serviceName = "RMI-register";
+    private static String registerServiceName = "RMI-REGISTER";
 
     public static void main(String []args) {
         if (args.length == 1) configurationFile = args[0];
@@ -25,20 +29,25 @@ public class ClientMain {
         RegisterInterface register = getRemoteRegisterObject();
 
         Scanner cliScanner = new Scanner(System.in);
-        String command = null;
-        while (command.equals("exit")) {
-            System.out.println(">");
+        String command;
+        boolean termination = false;
+
+        while (!termination) {
+            System.out.print("> ");
             command = cliScanner.nextLine();
 
             String []arguments = command.split(" ");
             switch (arguments[0]) {
-                case "register" : break;
+                case "register" : {
+                    registerUser(register, arguments);
+                    break;
+                }
                 case "login" : break;
                 case "logout" : break;
                 case "list" : {
                     if (arguments.length != 2 || (!arguments[1].equals("users") && !arguments[1].equals("following"))) {
                         System.err.println("Invalid command '" + command + "'. Use 'list users' or 'list following'");
-                        continue;
+                        break;
                     }
 
                     if (arguments[1].equals("users")) { //list users
@@ -46,6 +55,7 @@ public class ClientMain {
                     } else { //list following
 
                     }
+                    break;
                 }
                 case "follow" : break;
                 case "unfollow" : break;
@@ -54,7 +64,7 @@ public class ClientMain {
                 case "show" : {
                     if (arguments.length != 2 || (!arguments[1].equals("feed") && !arguments[1].equals("post"))) {
                         System.err.println("Invalid command '" + command + "'. Use 'show feed' or 'show post'");
-                        continue;
+                        break;
                     }
 
                     if (arguments[1].equals("feed")) { //show feed
@@ -62,6 +72,7 @@ public class ClientMain {
                     } else { //show post
 
                     }
+                    break;
                 }
                 case "delete" : break;
                 case "rewin" : break;
@@ -70,7 +81,7 @@ public class ClientMain {
                 case "wallet" : {
                     if (arguments.length > 2 || (arguments.length == 2 && !arguments[1].equals("btc"))) {
                         System.err.println("Invallid command '" + command + "'. Use 'wallet' or 'wallet btc'");
-                        continue;
+                        break;
                     }
 
                     if (arguments.length == 2) { //wallet btc
@@ -78,9 +89,37 @@ public class ClientMain {
                     } else { //wallet
 
                     }
-
+                    break;
                 }
+                case "exit" : { //TODO Scrivere nella relazione che si è voluto inserire questo comando per terminare il client
+                    termination = true;
+                    break;
+                }
+                default: System.err.println("Unknown command: " + command);
             }
+        }
+    }
+
+    //Registers a new user to WINSOME
+    private static void registerUser(RegisterInterface register, String[] arguments) {
+        if (arguments.length > 8) {
+            System.err.println("Usage: register username password tags (max 5 tag)");
+            return;
+        }
+
+        List<String> tag = new ArrayList<>(Arrays.asList(arguments).subList(3, arguments.length));
+
+        try {
+            int result;
+            if ((result = register.register(arguments[1], Hash.bytesToHex(Hash.sha256(arguments[2])), tag)) != 0) {
+                if (result == 1) System.err.println("Password field is empty");
+                if (result == 2 || result == 3) System.err.println("Registration requires minimum 1 tag and maximum 5");
+                if (result == 4) System.err.println("User '" + arguments[1] + "' already registered");
+            }
+        } catch (RemoteException e) {
+            System.err.println("Error while registering new user: " + e.getMessage());
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("Errore while hashing the password: " + e.getMessage());
         }
     }
 
@@ -88,7 +127,7 @@ public class ClientMain {
     private static RegisterInterface getRemoteRegisterObject() {
         try {
             Registry registry = LocateRegistry.getRegistry(registryPort);
-            return (RegisterInterface) registry.lookup(serviceName);
+            return (RegisterInterface) registry.lookup(registerServiceName);
         } catch (RemoteException e) {
             System.err.println("Error with the remote object: " + e.getMessage());
             System.exit(1);
@@ -108,11 +147,11 @@ public class ClientMain {
 
             while ((line = fileReader.readLine()) != null) {
                 switch (line.split("=")[0]) {
-                    case "REGISTRY-PORT" : registryPort = Integer.parseInt(line.split("=")[1]);
+                    case "REGISTRY-PORT" -> registryPort = Integer.parseInt(line.split("=")[1]);
 
-                    case "REGISTRY-HOST": registryHost = line.split("=")[1];
+                    case "REGISTRY-HOST" -> registryHost = line.split("=")[1];
 
-                    case "RMI-SERVICE": serviceName = line.split("=")[1];
+                    case "RMI-SERVICE" -> registerServiceName = line.split("=")[1];
                     //TODO Aggiungere ulteriori case
                 }
             }
