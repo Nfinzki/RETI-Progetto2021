b20dc1f5ad42b6e5df6f39e2b7f2f7e50a71ec3a
Nfinzki <francescodiluzio07@gmail.com>
2021-12-20 10:32:19 +0100

    Added json deserialization

--- src/Register.java
+++ src/Register.java
@@ -1,12 +1,25 @@
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.rmi.RemoteException;
 import java.util.*;
 
 public class Register implements RegisterInterface{
     private final Map<String, User> users;
+    private final Map<Integer, Post> posts;
+    private final String usersFile;
+    private final String postsFile;
 
-
-    public Register() {
+    public Register(String usersFile, String postsFile) {
         this.users = new HashMap<>();
+        this.posts = new HashMap<>();
+        this.usersFile = usersFile;
+        this.postsFile = postsFile;
+        readUsers();
+        readPosts();
     }
 
     public int register(String username, String password, List<String> tag) throws RemoteException {
@@ -27,5 +40,159 @@ public class Register implements RegisterInterface{
 
             return 4; //User already registered
         }
+    }
+
+    private Map<String, User> readUsers() {
+        Map<String, User> users = new HashMap<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(postsFile);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            JsonReader jsonReader = new JsonReader(inputStreamReader)) {
+                jsonReader.beginArray();
+
+                while (jsonReader.hasNext()) {
+                    jsonReader.beginObject();
+
+                    jsonReader.nextName();
+                    int id = jsonReader.nextInt();
+
+                    jsonReader.nextName();
+                    String postTitle = jsonReader.nextString();
+
+                    jsonReader.nextName();
+                    String postContent = jsonReader.nextString();
+
+                    jsonReader.nextName();
+                    jsonReader.beginArray();
+                    List<String> comments = new ArrayList<>();
+                    while (jsonReader.hasNext())
+                        comments.add(jsonReader.nextString());
+                    jsonReader.endArray();
+
+                    jsonReader.nextName();
+                    jsonReader.beginArray();
+                    List<Integer> upvotes = new ArrayList<>();
+                    while (jsonReader.hasNext())
+                        upvotes.add(jsonReader.nextInt());
+                    jsonReader.endArray();
+
+                    jsonReader.nextName();
+                    jsonReader.beginArray();
+                    List<Integer> downvotes = new ArrayList<>();
+                    while (jsonReader.hasNext())
+                        downvotes.add(jsonReader.nextInt());
+                    jsonReader.endArray();
+
+                    jsonReader.endObject();
+
+                    posts.put(
+                            id,
+                            new Post(
+                                    id,
+                                    postTitle,
+                                    postContent,
+                                    comments,
+                                    upvotes,
+                                    downvotes
+                            )
+                    );
+                }
+                jsonReader.endArray();
+
+            System.out.println(posts);
+        } catch (FileNotFoundException e) {
+            System.err.println("Error while reading json file: " + e.getMessage());
+            return null;
+        } catch (IOException e) {
+            System.err.println("I/O Error: " + e.getMessage());
+            return null;
+        }
+
+        return users;
+    }
+
+    private Map<Integer, Post> readPosts() {
+        Map<String, User> users = new HashMap<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(usersFile);
+             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+             JsonReader jsonReader = new JsonReader(inputStreamReader)) {
+            jsonReader.beginArray();
+
+            while (jsonReader.hasNext()) {
+                jsonReader.beginObject();
+
+                jsonReader.nextName();
+                int id = jsonReader.nextInt();
+
+                jsonReader.nextName();
+                String username = jsonReader.nextString();
+
+                jsonReader.nextName();
+                String password = jsonReader.nextString();
+
+                jsonReader.nextName();
+                jsonReader.beginArray();
+                List<String> tagList = new ArrayList<>();
+                while (jsonReader.hasNext())
+                    tagList.add(jsonReader.nextString());
+                String []tags = tagList.toArray(new String[0]);
+                jsonReader.endArray();
+
+                jsonReader.nextName();
+                jsonReader.beginArray();
+                List<Integer> follower = new ArrayList<>();
+                while (jsonReader.hasNext())
+                    follower.add(jsonReader.nextInt());
+                jsonReader.endArray();
+
+                jsonReader.nextName();
+                jsonReader.beginArray();
+                List<Integer> followed = new ArrayList<>();
+                while (jsonReader.hasNext())
+                    followed.add(jsonReader.nextInt());
+                jsonReader.endArray();
+
+                jsonReader.nextName();
+                jsonReader.beginArray();
+                List<Integer> blog = new ArrayList<>();
+                while (jsonReader.hasNext())
+                    blog.add(jsonReader.nextInt());
+                jsonReader.endArray();
+
+                jsonReader.nextName();
+                jsonReader.beginObject();
+                jsonReader.nextName();
+                double wincoin = jsonReader.nextDouble();
+                jsonReader.endObject();
+
+                jsonReader.endObject();
+
+                users.put(
+                        username,
+                        new User(
+                                id,
+                                username,
+                                password,
+                                tags,
+                                follower,
+                                followed,
+                                blog,
+                                new Wallet(wincoin)
+                        )
+                );
+            }
+            jsonReader.endArray();
+
+            System.out.println(users);
+        } catch (FileNotFoundException e) {
+            System.err.println("Error while reading json file: " + e.getMessage());
+            return null;
+        } catch (IOException e) {
+            System.err.println("I/O Error: " + e.getMessage());
+            return null;
+        }
+
+        return posts;
     }
 }
