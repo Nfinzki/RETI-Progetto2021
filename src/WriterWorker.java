fa14674f62aee9bd07c3569d97e95f89f14c53c6
Nfinzki <francescodiluzio07@gmail.com>
2021-12-23 11:17:07 +0100

    Separated workers for read operations and write operations

---
+++ src/WriterWorker.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+
+public class WriterWorker implements Runnable {
+    private final SelectionKey key;
+    private final SocketChannel client;
+    private final ByteBuffer byteBuffer;
+
+    private final Set<Registable> readyToBeRegistered;
+
+    private final Selector selector;
+
+    public WriterWorker(SelectionKey key, Set<Registable> readyToBeRegistered, Selector selector) {
+        this.key = key;
+        this.readyToBeRegistered = readyToBeRegistered;
+        this.selector = selector;
+
+        this.client = (SocketChannel) key.channel();
+        this.byteBuffer = (ByteBuffer) key.attachment();
+    }
+
+    public void run() {
+        sendResponse();
+        readyToBeRegistered.add(new Registable(client, SelectionKey.OP_READ, byteBuffer));
+        selector.wakeup();
+    }
+
+    private void sendResponse() {
+        try {
+            byteBuffer.flip();
+            client.write(byteBuffer);
+        } catch (IOException e) {
+            System.err.println("Error sending response to the client: " + e.getMessage());
+            try {key.channel().close();} catch (Exception ignored) {} //TODO Deve inviare comunque la risposta al client?
+        }
+    }
+}
