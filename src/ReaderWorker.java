7bcad5197f1eaa27ec16a10123a07830eba3cede
Nfinzki <francescodiluzio07@gmail.com>
2021-12-23 11:18:40 +0100

    Implemented logout

--- src/ReaderWorker.java
+++ src/ReaderWorker.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
+import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.nio.channels.NonReadableChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
@@ -8,24 +8,24 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.Set;
 
-public class ThreadWorker implements Runnable{
+public class ReaderWorker implements Runnable {
     private final SelectionKey key;
     private final SocketChannel client;
     private final ByteBuffer byteBuffer;
 
     private final Map<String, User> users;
     private final Map<Integer, Post> posts;
+    private final Map<String, Socket> loggedUsers;
     private final Set<Registable> readyToBeRegistered;
 
-    private final int operation;
     private final Selector selector;
 
-    public ThreadWorker(SelectionKey key, Map<String, User> users, Map<Integer, Post> posts, Set<Registable> readyToBeRegistered, int operation, Selector selector) {
+    public ReaderWorker(SelectionKey key, Map<String, User> users, Map<Integer, Post> posts, Map<String, Socket> loggedUsers, Set<Registable> readyToBeRegistered, Selector selector) {
         this.key = key;
         this.users = users;
         this.posts = posts;
+        this.loggedUsers = loggedUsers;
         this.readyToBeRegistered = readyToBeRegistered;
-        this.operation = operation;
         this.selector = selector;
 
         this.client = (SocketChannel) key.channel();
@@ -33,23 +33,18 @@ public class ThreadWorker implements Runnable{
     }
 
     public void run() {
-        if (operation == SelectionKey.OP_READ) {
-            String request = readRequest();
-            if (request == null) return;
+        String request = readRequest();
+        if (request == null) return;
 
-            String[] args = request.split(" ");
+        String[] args = request.split(" ");
 
-            switch (args[0]) {
-                case "login" -> login(args[1], args[2]);
-            }
-
-            readyToBeRegistered.add(new Registable(client, SelectionKey.OP_WRITE, byteBuffer));
-            selector.wakeup();
-        } else {
-            sendResponse();
-            readyToBeRegistered.add(new Registable(client, SelectionKey.OP_READ, byteBuffer));
-            selector.wakeup();
+        switch (args[0]) {
+            case "login" -> login(args[1], args[2]);
+            case "logout" -> logout(args[1]);
         }
+
+        readyToBeRegistered.add(new Registable(client, SelectionKey.OP_WRITE, byteBuffer));
+        selector.wakeup();
     }
 
     private void setResponse(int code) {
@@ -57,21 +52,13 @@ public class ThreadWorker implements Runnable{
         byteBuffer.putInt(code);
     }
 
-    private void sendResponse() {
-        try {
-            byteBuffer.flip();
-            client.write(byteBuffer);
-        } catch (IOException e) {
-            System.err.println("Error sending response to the client: " + e.getMessage());
-            try {key.channel().close();} catch (Exception ignored) {} //TODO Deve inviare comunque la risposta al client?
-        }
-    }
-
     private String readRequest() {
         try {
             byteBuffer.clear(); //Predispone la scrittura sul buffer
             client.read(byteBuffer); //Scrive dati sul buffer
             byteBuffer.flip();
+
+            if (byteBuffer.limit() == 0) return null; //TODO Rivedere questa cosa
 
             int requestLen = byteBuffer.getInt();
             byte[] requestBytes = new byte[requestLen];
@@ -86,11 +73,16 @@ public class ThreadWorker implements Runnable{
 
     private void login(String username, String password) {
         try {
+            if (loggedUsers.containsKey(username)) {
+                setResponse(2);
+                return;
+            }
             User user = users.get(username);
             if (user != null) {
-                if (user.comparePassword(Hash.bytesToHex(Hash.sha256(password))))
+                if (user.comparePassword(Hash.bytesToHex(Hash.sha256(password)))) {
+                    loggedUsers.put(username, client.socket());
                     setResponse(0);
-                else
+                } else
                     setResponse(1);
             } else {
                 setResponse(1);
@@ -99,5 +91,12 @@ public class ThreadWorker implements Runnable{
             System.err.println("Error while hashing the password: " + e.getMessage());
             try {client.close();} catch (Exception ignored) {} //TODO Deve inviare comunque la risposta al client?
         }
+    }
+
+    private void logout(String username) {
+        if (loggedUsers.remove(username) != null)
+            setResponse(0);
+        else
+            setResponse(1);
     }
 }
