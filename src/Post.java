097014ab4a9ed51656057f7b8a4a31e8a3b596a8
Nfinzki <francescodiluzio07@gmail.com>
2021-12-23 16:02:32 +0100

    Added authorId field

--- src/Post.java
+++ src/Post.java
@@ -4,14 +4,16 @@ import java.util.List;
 public class Post {
     private static int nextIdPost = 0;
     private final int idPost;
+    private final int authorId;
     private final String postTitle;
     private final String postContent;
     private final List<String> comments;
     private final List<Integer> upvotes;
     private final List<Integer> downvotes;
 
-    public Post(String postTitle, String postContent) {
+    public Post(int authorId, String postTitle, String postContent) {
         this.idPost = nextIdPost++;
+        this.authorId = authorId;
         this.postTitle = postTitle;
         this.postContent = postContent;
         comments = new ArrayList<>();
@@ -20,8 +22,9 @@ public class Post {
 
     }
 
-    public Post(int idPost, String postTitle, String postContent, List<String> comments, List<Integer> upvotes, List<Integer> downvotes) {
+    public Post(int idPost, int authorId, String postTitle, String postContent, List<String> comments, List<Integer> upvotes, List<Integer> downvotes) {
         this.idPost = idPost;
+        this.authorId = authorId;
         this.postTitle = postTitle;
         this.postContent = postContent;
         this.comments = comments;
