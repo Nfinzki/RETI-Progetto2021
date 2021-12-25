07dbf1e56c8e76bb413d6768f63b196a91c77d26
Nfinzki <francescodiluzio07@gmail.com>
2021-12-25 20:39:39 +0100

    Added method getFollowersAdJson

--- src/User.java
+++ src/User.java
@@ -1,9 +1,9 @@
+import com.google.gson.Gson;
+
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class User {
-    //public static int nextId = 0;
-    //private final int id;
     private final String username;
     private final String password;
     private final String []tag;
@@ -16,12 +16,9 @@ public class User {
     public User(String username, String password, String []tag) {
         if (tag.length > 5) throw new ArrayIndexOutOfBoundsException();
 
-        //this.id = nextId++;
         this.username = username;
         this.password = password;
         this.tag = tag;
-        //this.follower = new ArrayList<>();
-        //this.followed = new ArrayList<>();
         this.follower = ConcurrentHashMap.newKeySet();
         this.followed = ConcurrentHashMap.newKeySet();
         this.blog = ConcurrentHashMap.newKeySet();
@@ -29,7 +26,6 @@ public class User {
     }
 
     public User(String username, String password, String []tag, Set<String> follower, Set<String> followed, Set<Integer> blog, Wallet wallet) {
-        //this.id = id;
         this.username = username;
         this.password = password;
         this.tag = tag;
@@ -37,17 +33,11 @@ public class User {
         this.followed = followed;
         this.blog = blog;
         this.wallet = wallet;
-
-        //if (id >= nextId) nextId = id + 1;
     }
 
     public boolean comparePassword(String password) {
         return this.password.equals(password);
     }
-
-    /*public int getId() {
-        return id;
-    }*/
 
     public String getUsername() {
         return username;
@@ -98,6 +88,11 @@ public class User {
 
     public boolean removeFollower(String follower) {
         return this.follower.remove(follower);
+    }
+
+    public String getFollowersAsJson() {
+        Gson gson = new Gson();
+        return gson.toJson(follower);
     }
 
     public String toString() {
