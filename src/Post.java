65807d27b0af6274a2d1859cd351bfcc5a827061
Nfinzki <francescodiluzio07@gmail.com>
2021-12-27 18:49:26 +0100

    Added methods to add comment and to get post as json string

--- src/Post.java
+++ src/Post.java
@@ -11,7 +11,7 @@ public class Post {
     private final String author;
     private final String postTitle;
     private final String postContent;
-    private final List<String> comments;
+    private final Set<Comment> comments;
     private final Set<String> upvotes;
     private final Set<String> downvotes;
 
@@ -20,13 +20,13 @@ public class Post {
         this.author = author;
         this.postTitle = postTitle;
         this.postContent = postContent;
-        comments = new ArrayList<>();
+        comments = ConcurrentHashMap.newKeySet();
         upvotes = ConcurrentHashMap.newKeySet();
         downvotes = ConcurrentHashMap.newKeySet();
 
     }
 
-    public Post(int idPost, String author, String postTitle, String postContent, List<String> comments, Set<String> upvotes, Set<String> downvotes) {
+    public Post(int idPost, String author, String postTitle, String postContent, Set<Comment> comments, Set<String> upvotes, Set<String> downvotes) {
         this.idPost = idPost;
         this.author = author;
         this.postTitle = postTitle;
@@ -54,6 +54,10 @@ public class Post {
         return downvotes.add(user);
     }
 
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+    }
+
     public boolean containsUpvote(String user) {
         return upvotes.contains(user);
     }
@@ -62,9 +66,22 @@ public class Post {
         return downvotes.contains(user);
     }
 
-    public String toJson() { //TODO Deve inviare solo idPost, Autore e Titolo
-        Gson gson = new Gson();
-        return gson.toJson(this);
+    public String toJson() {
+        String serializedPost = "{\"postTitle\":\"" + postTitle + "\", \"postContent\":\"" + postContent + "\", \"upvotes\":" + upvotes.size() + ", \"downvotes\":" + downvotes.size() + ", \"comments\": [";
+        boolean firstEntry = true;
+        for (Comment comment : comments) {
+            if (!firstEntry) serializedPost += ", ";
+            serializedPost += comment.toJson();
+
+            firstEntry = false;
+        }
+        serializedPost += "]}";
+
+        return serializedPost;
+    }
+
+    public String basicInfoToJson() {
+        return "{\"idPost\": " + idPost + ", \"author\": \"" + author + "\", \"postTitle\": \"" + postTitle + "\" }";
     }
 
     public String toString() {
