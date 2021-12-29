993613e477b896ce6fa04aea2e948ac97f688f7e
Nfinzki <francescodiluzio07@gmail.com>
2021-12-29 21:55:14 +0100

    Added class to receive multicast notification

---
+++ src/NotifyHandler.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class NotifyHandler implements Runnable{
+    private final String multicastIP;
+    private final int multicastPort;
+
+    private final int bufferSize = 512;
+
+    public NotifyHandler(String multicastIP, int multicastPort) {
+        this.multicastIP = multicastIP;
+        this.multicastPort = multicastPort;
+    }
+
+    public void run() {
+        try (MulticastSocket multicastSocket = new MulticastSocket(multicastPort)) {
+            InetAddress address = InetAddress.getByName(multicastIP);
+            if (!address.isMulticastAddress()) throw new IllegalArgumentException("Not a multicast address: " + multicastIP);
+
+            multicastSocket.joinGroup(address);
+
+            byte []messaggeBuffer = new byte[bufferSize];
+            DatagramPacket packet = new DatagramPacket(messaggeBuffer, messaggeBuffer.length);
+
+            while (!Thread.currentThread().isInterrupted()) {
+                multicastSocket.receive(packet);
+                String notification = new String(packet.getData(), 0, packet.getLength());
+                System.out.println(notification);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
