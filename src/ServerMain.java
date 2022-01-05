bcd76582a29e6f43d7a5950fe8baf54116911a34
Nfinzki <francescodiluzio07@gmail.com>
2022-01-05 21:18:42 +0100

    Terminated config file parsing + some commenting

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -19,13 +19,10 @@ public class ServerMain {
 
     private static String serverIP = "localhost";
     private static int tcpPort = 2222;
-    private static int udpPort = 3333;
     public static String multicastIP = "239.255.32.32";
     public static int multicastPort = 4444;
-    private static String registryHost = "localhost";
     private static int registryPort = 55555;
     private static String registerServiceName = "RMI-REGISTER";
-    private static int socketTimeout = 60000;
     private static int bufferSize = 16 * 1024;
 
     private static int corePoolSize = 5;
@@ -41,7 +38,7 @@ public class ServerMain {
     private static String callbackHandlerService = "RMI-FOLLOWER-CALLBACK";
 
     private static int calculationTime = 25000;
-    private static int authorPercentage = 50;
+    private static double authorPercentage = 50;
 
     private static final AtomicBoolean stateChanged = new AtomicBoolean(false);
     private static int saveStateTime = 15 * 6000;
@@ -55,14 +52,18 @@ public class ServerMain {
             System.exit(1);
         }
 
+        //Parses the configuration file
         parseConfigFile();
 
+        //Initializes the state of the server
         users = new ConcurrentHashMap<>();
         posts = new ConcurrentHashMap<>();
         loggedUsers = new ConcurrentHashMap<>();
 
+        //Initializes the Collection to handle reads and writes from/for a client
         readyToBeRegistered = ConcurrentHashMap.newKeySet();
 
+        //Recovers the state of the server
         RecoverState.readUsers(users, usersFile);
         RecoverState.readPosts(posts, postsFile);
 
@@ -71,21 +72,28 @@ public class ServerMain {
 
         List<Thread> activeThread = new ArrayList<>();
 
+        //Creates and starts the thread that calculates the revenue
         Thread revenueThread = new Thread(new RevenueCalculator(users, posts, calculationTime, authorPercentage, multicastIP, multicastPort, stateChanged));
         revenueThread.start();
         activeThread.add(revenueThread);
 
+        //Creates and starts the thread that periodically saves the state of the server
         Thread saveStateThread = new Thread(new SaveState(users, posts, usersFile, postsFile, stateChanged, saveStateTime));
         saveStateThread.start();
         activeThread.add(saveStateThread);
 
+        //Creates and starts the thread that periodically logs out inactive users logged in
         Thread automaticLogoutThread = new Thread(new AutomaticLogoutHandler(loggedUsers, automaticLogoutCheckTime));
         automaticLogoutThread.start();
         activeThread.add(automaticLogoutThread);
 
+        //Creates the ThreadPool to handle clients requests
         ThreadPoolExecutor threadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
 
+        //Creates the ShutdownHook to terminate the server correctly
         ShutdownHandler shutdownHandler = new ShutdownHandler(usersFile, postsFile, users, posts, threadPool, activeThread, stateChanged);
+
+        //Opens the server
         multiplexChannels(threadPool);
     }
 
@@ -186,28 +194,123 @@ public class ServerMain {
                 switch (line.split("=")[0]) {
                     case "SERVER-IP" -> serverIP = line.split("=")[1];
 
-                    case "TCP-PORT" -> tcpPort = Integer.parseInt(line.split("=")[1]);
+                    case "TCP-PORT" -> {
+                        tcpPort = Integer.parseInt(line.split("=")[1]);
 
-                    case "UDP-PORT" -> udpPort = Integer.parseInt(line.split("=")[1]);
+                        if (tcpPort < 0 || tcpPort > 65535) {
+                            System.err.println("TCP-PORT must be between 0 and 65535");
+                            System.exit(1);
+                        }
+                    }
 
                     case "MULTICAST-IP" -> multicastIP = line.split("=")[1];
 
-                    case "MCAST-PORT" -> multicastPort = Integer.parseInt(line.split("=")[1]);
+                    case "MCAST-PORT" -> {
+                        multicastPort = Integer.parseInt(line.split("=")[1]);
 
-                    case "REGISTRY-HOST" -> registryHost = line.split("=")[1];
+                        if (multicastPort < 0 || multicastPort > 65535) {
+                            System.err.println("MULTICAST-PORT must be between 0 and 65535");
+                            System.exit(1);
+                        }
+                    }
 
-                    case "REGISTRY-PORT" -> registryPort = Integer.parseInt(line.split("=")[1]);
+                    case "REGISTRY-PORT" -> {
+                        registryPort = Integer.parseInt(line.split("=")[1]);
 
-                    case "SOCKET-TIMEOUT" -> socketTimeout = Integer.parseInt(line.split("=")[1]);
+                        if (registryPort < 0 || registryPort > 65535) {
+                            System.err.println("REGISTRY-PORT must be between 0 and 65535");
+                            System.exit(1);
+                        }
+                    }
 
-                    case "RMI-SERVICE" -> registerServiceName = line.split("=")[1];
+                    case "BUFFER-SIZE" -> {
+                        bufferSize = Integer.parseInt(line.split("=")[1]);
 
-                    case "POOL-SIZE" -> corePoolSize = Integer.parseInt(line.split("=")[1]);
+                        if (bufferSize < 0) {
+                            System.err.println("BUFFER-SIZE cannot be negative");
+                            System.exit(1);
+                        }
+                    }
 
-                    case "MAX-POOL-SIZE" -> maximumPoolSize = Integer.parseInt(line.split("=")[1]);
+                    case "RMI-REGISTER-SERVICE" -> registerServiceName = line.split("=")[1];
 
-                    case "KEEPALIVE" -> keepAliveTime = Integer.parseInt(line.split("=")[1]);
+                    case "POOL-SIZE" -> {
+                        corePoolSize = Integer.parseInt(line.split("=")[1]);
+
+                        if (corePoolSize < 0) {
+                            System.err.println("POOL-SIZE cannot be negative");
+                            System.exit(1);
+                        }
+                    }
+
+                    case "MAX-POOL-SIZE" -> {
+                        maximumPoolSize = Integer.parseInt(line.split("=")[1]);
+
+                        if (maximumPoolSize < 0) {
+                            System.err.println("MAX-POOL-SIZE cannot be negative");
+                            System.exit(1);
+                        }
+                    }
+
+                    case "KEEPALIVE" -> {
+                        keepAliveTime = Integer.parseInt(line.split("=")[1]);
+
+                        if (keepAliveTime < 0) {
+                            System.err.println("KEEPALIVE cannot be negative");
+                            System.exit(1);
+                        }
+                    }
+
+                    case "RMI-FOLLOWER-SERVICE" -> callbackHandlerService = line.split("=")[1];
+
+                    case "REVENUE-TIME" -> {
+                        calculationTime = Integer.parseInt(line.split("=")[1]);
+
+                        if (calculationTime < 0) {
+                            System.err.println("REVENUT-TIME cannot be negative");
+                            System.exit(1);
+                        }
+                    }
+
+                    case "AUTHOR-PERCENTAGE" -> {
+                        authorPercentage = Double.parseDouble(line.split("=")[1]);
+
+                        if (authorPercentage < 0 | authorPercentage > 100) {
+                            System.err.println("AUTHOR-PERCENTAGE must be between 0 and 100");
+                            System.exit(1);
+                        }
+                    }
+
+                    case "BACKUP-TIME" -> {
+                        saveStateTime = Integer.parseInt(line.split("=")[1]);
+
+                        if (saveStateTime < 0) {
+                            System.err.println("BACKUP-TIME cannot be negative");
+                            System.exit(1);
+                        }
+                    }
+
+                    case "AUTOMATIC-LOGOUT" -> {
+                        automaticLogoutCheckTime = Integer.parseInt(line.split("=")[1]);
+
+                        if (automaticLogoutCheckTime < 0) {
+                            System.err.println("AUTOMATIC-LOGOUT cannot be negative");
+                            System.exit(1);
+                        }
+                    }
+
+                    default -> {
+                        if (!line.equals("") && !line.startsWith("#")) {
+                            System.err.println("Invalid option: " + line);
+                            System.exit(1);
+                        }
+                    }
                 }
+            }
+
+            if (maximumPoolSize < corePoolSize) {
+                System.err.println("MAX-POOL-SIZE must be grater or equal then POOL-SIZE");
+                System.exit(1);
             }
 
         } catch (IOException e) {
